c3763150a6547f73ff22bdd274490cb9ecc46cb5
Iago Riveiro Santos Dutra <iagorivsd@gmail.com>
2023-03-17 13:33:15 -0300

    Solve drills_14_mar

--- drills_14_mar.py
+++ drills_14_mar.py
@@ -11,57 +11,49 @@ import pandas as pd
 import cv2
 import math
 
-# img = cv2.imread('1200px-Palazzo_Farnese_Fassade.jpg')
+def main():
+    problem_one()
+    problem_two()
+    problem_three()
 
-# #rotate by 30 degrees
-# rotate = np.array([[math.cos(np.pi/6), -math.sin(np.pi/6)], [math.sin(np.pi/6), math.cos(np.pi/6)]])
+def problem_one():
+    img = cv2.imread('palazzo.jpg')
+    cv2.imshow('Input', img)
+    cv2.waitKey()
 
-# print(img)
+def problem_two():
+    # Missing interpolation
+    img = cv2.imread('palazzo.jpg')
+    rot_matrix = np.array([[math.cos(np.pi/6), -math.sin(np.pi/6)], [math.sin(np.pi/6), math.cos(np.pi/6)]])
+    canvas = np.zeros([2*img.shape[0], 2*img.shape[1], 3])
 
-# imagem2 = np.zeros((2*img.shape[0], 2*img.shape[1], 3), np.uint8)
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            pos_vec = np.array([i, j])
+            pos_vec = np.matmul(rot_matrix, pos_vec)
+            pos_vec = np.floor(pos_vec).astype(int)
 
-# for i in range(img.shape[0]):
-#     for j in range(img.shape[1]):
-#         x = np.array([i, j])
-#         x = np.dot(x, rotate)
-#         x = np.floor(x).astype(int)
-#         imagem2[x[0], x[1]+img.shape[1]] = img[i, j]
+            # Has to be corrected
+            x_padding = round(canvas.shape[0]/2 - img.shape[0]*math.cos(np.pi/3))
+            y_padding = round(canvas.shape[1]/2 - img.shape[1]*math.sin(np.pi/6))
 
-# cv2.imwrite("changed.jpg", imagem2)
-
-
-
-# img1 = cv2.imread('1200px-Palazzo_Farnese_Fassade.jpg')
-# rows, cols = img1.shape[:2]
-
-# src_points = np.float32([[0,0], [cols-1,0], [0,rows-1], [cols-1,rows-1]])
-# dst_points = np.float32([[0,0], [cols-1,0], [int(0.33*cols),rows-1], [int(0.66*cols),rows-1]])
-
-# src_points = np.float32([[0,0], [0,rows-1], [cols/2,0], [cols/2,rows-1]])
-# dst_points = np.float32([[0,100], [0,rows-101], [cols/2,0], [cols/2,rows-1]])
-
-# projective_matrix = cv2.getPerspectiveTransform(src_points, dst_points)
-# img_output = cv2.warpPerspective(img1, projective_matrix, (cols,rows))
-
-# cv2.imshow('Input', img1)
-# cv2.imshow('Output', img_output)
-# cv2.waitKey()
-
-
-img = cv2.imread('1200px-Palazzo_Farnese_Fassade.jpg')
-
-#perpective from left to right
-
-
-perpective_matrix = np.array([[1, 0, 100], [0, 1, -100], [100,-100,1]])
-
-imagem2 = np.zeros((3*img.shape[0], 3*img.shape[1], 3), np.uint8)
-
-for i in range(img.shape[0]):
-    for j in range(img.shape[1]):
-        x = np.array([i, j, 1])
-        x = np.matmul(x, perpective_matrix)
-        x = np.floor(x).astype(int)
-        imagem2[x[0], x[1]] = img[i, j]
+            canvas[pos_vec[0] + x_padding, pos_vec[1] + y_padding] = img[i, j]
     
-cv2.imwrite("change.jpg", imagem2)
+    cv2.imwrite("rotated_palazzo.jpg", canvas)
+
+def problem_three():
+    img = cv2.imread('palazzo.jpg')
+    rows, cols = img.shape[:2]
+
+    src_points = np.float32([[0,0], [cols-1,0], [0,rows-1], [cols-1,rows-1]])
+    dst_points = np.float32([[0,0], [cols-1,0], [int(0.33*cols),rows-1], [int(0.66*cols),rows-1]])
+
+    src_points = np.float32([[0,0], [0,rows-1], [cols/2,0], [cols/2,rows-1]])
+    dst_points = np.float32([[0,100], [0,rows-101], [cols/2,0], [cols/2,rows-1]])
+
+    proj_matrix = cv2.getPerspectiveTransform(src_points, dst_points)
+    img_output = cv2.warpPerspective(img, proj_matrix, (cols,rows))
+    cv2.imwrite('perspective_palazzo.jpg', img_output)
+
+if __name__ == "__main__":
+    main()
